6afeb4a01112225152e559e1a53f9766a80b8166
Enzu83 <enzu.mc@gmail.com>
2025-07-08 00:17:09 +0200

    feat: rom header print

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    // open the ROM and store the content in a byte vector
+    let mut f = File::open("roms/mario.nes").expect("No file found at given path.");
+    let mut data = vec![];
+    f.read_to_end(&mut data).expect("Error while reading file.");
+
+    // get the first three elements of the ROM
+    let header = &data[..3];
+    let header: Vec<char> = header.iter().map(|&b| b as char).collect();
+
+    // the header should be "NES"
+    println!("{}", header.iter().collect::<String>());
 }
