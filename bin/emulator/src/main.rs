14bff7d18771859169a5b404340024d1c0265a8b
Enzu83 <enzu.mc@gmail.com>
2026-01-04 04:41:50 +0100

    refactor: change project structure for a more standard rust project

---
+++ bin/emulator/src/main.rs
@@ -0,0 +1,23 @@
+use std::env::args;
+use std::fs::File;
+use std::io::Read;
+
+use anyhow::{Result, bail};
+
+fn main() -> Result<()> {
+    let path = match args().skip(1).next() {
+        Some(arg) => arg,
+        None =>  bail!("Please provide a path to the ROM."),
+    };
+
+    // open the ROM and store the content in a byte vector
+    let mut rom = Vec::new();
+    File::open(path)?.read_to_end(&mut rom)?;
+
+    // get the first three elements of the ROM
+    if std::str::from_utf8(&rom[..3])? != "NES" {
+        bail!("ROM header is incorrect")
+    };
+
+    Ok(())
+}
